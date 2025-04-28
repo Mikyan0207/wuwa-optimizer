c95f8c0769a84c8825036229b0d8b2f8f3e2834f
Mikyan <mikyan0207@gmail.com>
2025-04-28 15:59:27 +0900

    Update EchoIcon to match the current UI

--- app/pages/echoes/index.vue
+++ app/pages/echoes/index.vue
@@ -4,7 +4,7 @@ const EchoesStore = useEchoesStore()
 
 <template>
   <div>
-    <div class="mx-auto xl:max-w-6xl px-8 xl:px-0 flex items-center justify-center gap-2">
+    <div class="mx-auto xl:max-w-7xl px-8 xl:px-0 flex flex-wrap items-start justify-center gap-2">
       <EchoIcon
         v-for="e in EchoesStore.GetEchoes()"
         :key="e.Id"

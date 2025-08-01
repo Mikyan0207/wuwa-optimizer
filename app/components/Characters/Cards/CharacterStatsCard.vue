cf980096e9ed743c53e1ab584f622c791ae26d43
Mikyan <mikyan0207@gmail.com>
2025-08-01 16:04:00 +0900

    Update Characters stats weights

--- app/components/Characters/Cards/CharacterStatsCard.vue
+++ app/components/Characters/Cards/CharacterStatsCard.vue
@@ -22,11 +22,6 @@ function IsStatWanted(stat: IStatistic) {
     return ''
   }
 
-  if (w === 0.1) {
-    // This is a special case for ELEMENT DMG BONUS
-    return 'text-amber-500'
-  }
-
   switch (true) {
     case (w > 0 && w <= 0.25):
       return 'text-green-500'
@@ -65,17 +60,21 @@ function IsStatWanted(stat: IStatistic) {
     <div class="mx-auto my-4 h-[1px] w-full rounded-full bg-white/14" />
     <div class="flex flex-col">
       <div class="w-full flex flex-col items-start gap-1 relative">
-        <StatLine
+        <div
           v-for="(st, idx) in Stats"
           :key="`${st.Type}-${st.Value}`"
           v-motion-slide-left
           :delay="500 + (idx * 50)"
-          :stat="st"
-          :show-line="true"
-          :is-wanted-color="IsStatWanted(st)"
-          class="px-2 py-1"
+          class="w-full flex items-center justify-between px-2 py-1"
           :class="{ 'bg-neutral-800/75 rounded': CurrentCharacter.StatsWeights![st.Type] !== undefined && CurrentCharacter.StatsWeights![st.Type] || 0 > 0 }"
-        />
+        >
+          <StatLine
+            :stat="st"
+            :show-line="true"
+            :is-wanted-color="IsStatWanted(st)"
+            class="flex-1"
+          />
+        </div>
       </div>
       <div class="mx-auto my-4 h-[1px] w-full rounded-full bg-white/14" />
       <div v-motion-slide-bottom :delay="500" class="w-full flex text-lg items-center justify-evenly">
